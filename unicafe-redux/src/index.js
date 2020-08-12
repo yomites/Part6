7c0908d6d557b6503b044b1674dcae2008633c17
yomites <marshall_400@yahoo.com>
2020-08-12 20:54:36 +0300

    Exercise 6.2
    Exercise 6.2: unicafe revisited step2. Implementing the actual functionality of the application

--- unicafe-redux/src/index.js
+++ unicafe-redux/src/index.js
@@ -10,7 +10,47 @@ store.subscribe(() => {
   console.log('Store Now', storeNow)
 })
 
+const Statistics = ({ all, good, neutral, bad, average, positive }) => {
+  if (all === 0) {
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+  }
+
+  return (
+    <table>
+      <tbody>
+        <Statistic text="good" value={good} />
+        <Statistic text="neutral" value={neutral} />
+        <Statistic text="bad" value={bad} />
+        <Statistic text="all" value={all} />
+        <Statistic text="average" value={average} />
+        <Statistic text="positive" value={positive} />
+      </tbody>
+    </table>
+  )
+}
+
+const Statistic = ({ text, value }) => {
+  return (
+    <tr>
+      <td>
+        {text}
+      </td>
+      <td>
+        {value}
+      </td>
+    </tr>
+  )
+}
+
 const App = () => {
+
+  const feedback = "give feedback"
+  const statistics = "statistics"
+
   const good = () => {
     store.dispatch({
       type: 'GOOD'
@@ -35,15 +75,28 @@ const App = () => {
     })
   }
 
+  const all = store.getState().good + store.getState().ok + store.getState().bad
+  const feedbackValues = (store.getState().good * 1) + (store.getState().ok * 0) + (store.getState().bad * -1)
+  const average = (feedbackValues / all)
+  const positive = (store.getState().good * 100 / all)
+  const positiveWithPercentSign = positive + " %"
+
   return (
     <div>
-      <button onClick={good}>good</button> 
-      <button onClick={neutral}>neutral</button> 
+      <h1>{feedback}</h1>
+      <button onClick={good}>good</button>
+      <button onClick={neutral}>neutral</button>
       <button onClick={bad}>bad</button>
       <button onClick={reset}>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>neutral {store.getState().ok}</div>
-      <div>bad {store.getState().bad}</div>
+      <h2>{statistics}</h2>
+      <Statistics
+        good={store.getState().good}
+        neutral={store.getState().ok}
+        bad={store.getState().bad}
+        all={all}
+        average={average}
+        positive={positiveWithPercentSign}
+      />
     </div>
   )
 }
